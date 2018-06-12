7a30c8c83583c790269ba110fc1160d81469d627
Leonardo <sonofspardaleo@hotmail.com>
2018-06-12 14:59:37 -0300

    Add files via upload

---
+++ Desafio 6.py
@@ -0,0 +1,10 @@
+n = int(input('Digite um valor: '))
+x = n*2
+y = n*3
+z = n**(1/2)
+
+print('O dobro de {} é {}'.format(n, x))
+
+print('O triplo de {} é {}'.format(n, y))
+
+print('A raiz quadrada de {} é {}'.format(n, z))
---
+++ Desafio 7.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite sua 1° nota: '))
+n2 = float(input('Digite sua 2° nota: '))
+
+m = (n1+n2)/2
+
+print('Média: {:.1f}'.format(m))
